dc4e45d0961465fe6e74d32b1e4f60ae4670df94
BohdanMelvic <bohdanmelvic@gmail.com>
2025-03-22 16:01:30 +0200

    add categories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import Directory from './components/directory/directory'
 
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://picsum.photos/200/100',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://picsum.photos/200/200',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://picsum.photos/200/300',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'https://picsum.photos/200/400',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'https://picsum.photos/200/500',
+    },
+  ];
 
-const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Store
-      </header>
-    </div>
+    <Directory categories={categories} />    
   );
-}
+};
 
 export default App;
