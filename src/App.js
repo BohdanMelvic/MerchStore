b56cf89dbb98922f5b428f59a7e5a15cffc4cc53
BohdanMelvic <bohdanmelvic@gmail.com>
2025-03-23 20:01:33 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import Directory from './components/directory/directory'
+import Home from './routes/home/home';
+import { Routes, Route } from "react-router";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl: 'https://picsum.photos/200/100',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl: 'https://picsum.photos/200/200',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl: 'https://picsum.photos/200/300',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-      imageUrl: 'https://picsum.photos/200/400',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-      imageUrl: 'https://picsum.photos/200/500',
-    },
-  ];
 
   return (
-    <Directory categories={categories} />    
+    <Routes>
+      <Route path="/" element={<Home  /> } />
+    </Routes>   
   );
 };
 
