444941eb5672591c067ea239354569f22dec2a35
BohdanMelvic <bohdanmelvic@gmail.com>
2025-03-29 10:59:14 +0200

    added sig-in form

--- src/routes/sign-in/signIn.js
+++ src/routes/sign-in/signIn.js
@@ -1,17 +1,23 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase';
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase.js';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form';
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-      const { user } = await signInWithGooglePopup();
-      const userDocRef = await createUserDocumentFromAuth(user);
-    };
-  
-    return (
-      <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      </div>
-    );
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
-export default SignIn
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignIn;
